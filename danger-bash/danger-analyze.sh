11dfc00f11b6bf1f3ffa6f3f41e49dde9b323be7
owner <owner@MacBook-Pro-6.local>
2025-07-08 20:32:04 -0400

    updated workflow

--- danger-bash/danger-analyze.sh
+++ danger-bash/danger-analyze.sh
@@ -109,7 +109,7 @@ EOF
     log "DEBUG" "sandeep appending result"
     # Append result to results array
     results+=("$result")
-
+    log "DEBUG" "sandeep appending result 2"
     # Update counters
     case $severity in
         error) ((errors++)) ;;
