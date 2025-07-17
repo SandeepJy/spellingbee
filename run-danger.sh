e699172904a91c257d5f6cceb72414994d0dccb8
owner <owner@MacBook-Pro-6.local>
2025-07-17 15:34:20 -0400

    Updates

--- run-danger.sh
+++ run-danger.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 # Configuration
-DANGER_VERSION="${DANGER_VERSION:-v5.5}"
+DANGER_VERSION="${DANGER_VERSION:-v5.6}"
 DANGER_REPO_NAME="danger-core"
 DANGER_REPO="https://github.com/SandeepJy/${DANGER_REPO_NAME}"
 DANGER_DIR="Danger"
