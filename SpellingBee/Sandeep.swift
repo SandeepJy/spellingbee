6ca3bf6f327664cc8add8fe5f048af1336057917
Sandeep Jyoti <sandeepjyoti@gmail.com>
2025-08-27 19:43:08 -0400

    added new file sandeep.swift; force unwrap

---
+++ SpellingBee/Sandeep.swift
@@ -0,0 +1,14 @@
+import something
+
+struct Sandeep {
+    var text = "Hello, World!"
+    var str: String?
+
+    let j = str!
+}
+
+struct Sandeep_Previews: PreviewProvider {
+    static var previews: some View {
+        Sandeep()
+    }
+}
